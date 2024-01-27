85620985b2f141267fd468084d3f88ecf1f64701
Nagpal45 <nagpal4503@gmail.com>
2024-01-27 13:41:10 +0530

    add oauth user to db

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,7 +5,6 @@ import { auth } from "@/lib/auth";
 
 export default async function Navbar() {
   const session = await auth();
-  console.log(session);
   return (
     <div className={styles.container}>
     <Link href="/" className={styles.logo}>devVN</Link>
